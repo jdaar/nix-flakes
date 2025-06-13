34c2214d54051effb16af16d2d3bd14782338027
jhonatan <jhonatandaar@gmail.com>
2025-06-13 11:52:59 -0500

    chore: added aider flake

---
+++ aider/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "aider";
+
+  inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/c2a03962b8e2";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    perSystem = system: let pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+					(pkgs.aider-chat.withOptional {
+						withPlaywright = true;
+						withBrowser = true;
+						withHelp = true;
+						withBedrock = true;
+						withAll = true;
+					})
+        ];
+        shellHook = ''
+          echo "aider environment"
+					if [[ -d "./.venv" ]]; then
+						source ./.venv/bin/activate
+						export AIDER_MODEL=o3-mini
+						export AIDER_OPENAI_API_KEY=GETYOUROWNKEY
+					fi	
+        '';
+      };
+    };
+    allSystemsOutputs = forAllSystems perSystem;
+  in {
+    devShells = forAllSystems (system: {
+      default = allSystemsOutputs.${system}.devShell;
+    });
+  };
+}
